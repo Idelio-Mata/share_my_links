936528212688ba21c5a64c0ff1f4f01384609683
Idelio-Mata <idelioteofilomata@gmail.com>
2022-12-12 15:02:13 +0200

    Links created.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,38 @@
+import './home.css'
 export default function Home(){
   return(
-    <div>
-      <h1>Home Page</h1>
+    <div className='home-container'>
+      <h1>Idélio Mata</h1>
+      <span> See my links 👇🏾</span>
+
+      <main className='links'>
+        
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'> My Linkedin Profile</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'> My Facebook Profile</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'> My GitHub Profile</p>
+          </a>
+        </section>
+
+        
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'> My Telegram Profile</p>
+          </a>
+        </section>
+      </main>
+      
     </div>
   )
 }

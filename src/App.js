55c30ff99852abf8647fd0f2f081642bf4ed7be0
Idelio-Mata <idelioteofilomata@gmail.com>
2022-12-12 13:16:22 +0200

    Error Page Customized

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {createBrowserRouter} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Adminn from './pages/Admin'
-
 import Error from './pages/Error'
 
 const router = createBrowserRouter([
--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,8 +1,19 @@
+import './error.css'
+
+import { Link } from 'react-router-dom'
+import { Logo } from '../../components/Logo'
+
 export default function Error(){
   return(
-    <div>
+    <div className='error'>
+      <Logo/>
       <h1>Page not found!</h1>
-      <h3>This page is not available.</h3>
+      <p>The page you'are looking for is not available.</p>
+      
+      <Link to="/" className='link'>
+        Back to Home page!
+      </Link>
+
     </div>
   )
 }

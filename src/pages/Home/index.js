65f680e57d7fef21b10ecdedaad7a0cced960bed
Idelio-Mata <idelioteofilomata@gmail.com>
2022-12-13 16:41:57 +0200

    Icons & Responsivity

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,9 @@
 import './home.css'
+
+import { Social } from '../../components/Social'
+
+import {FaFacebook, FaGithub, FaLinkedin, FaLinkedinIn, FaWhatsapp} from 'react-icons/fa'
+
 export default function Home(){
   return(
     <div className='home-container'>
@@ -31,6 +36,22 @@ export default function Home(){
             <p className='link-text'> My Telegram Profile</p>
           </a>
         </section>
+
+        <footer>
+
+          <Social url="https://github.com/Idelio-Mata">
+            <FaGithub size={35} color="#fff"/>
+          </Social>
+
+          <Social url="https://whatsa.me/25825845701361/?t=Message%20to%20Id%C3%A9lio_Mata">
+            <FaWhatsapp size={35} color="#fff"/>
+          </Social>
+
+          <Social url=" https://www.linkedin.com/in/id%C3%A9liote%C3%B3filomata/">
+            <FaLinkedinIn size={34} color="#fff"/>
+          </Social>
+         
+        </footer>
       </main>
       
     </div>

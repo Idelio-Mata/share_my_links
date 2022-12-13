afecd12c38a4be61d8580d3ef2799a53c948d60b
Idelio-Mata <idelioteofilomata@gmail.com>
2022-12-13 17:56:29 +0200

    Working in useState

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import './login.css'
 
 import { Logo} from '../../components/Logo'
 
 export default function Login(){
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+
+
   return(
     <div className='login-container'>
       <Logo/>
@@ -12,11 +18,15 @@ export default function Login(){
         <input 
           type='email' 
           placeholder='Type your email...'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         
         <input
           type='password'
           placeholder='********' autocomplete='on'
+          value={password}
+          onChange ={(e) => setPassword(e.target.value)}
         />
 
         <button type='submit'>Login</button>

49bdb6280856d90882e1fb9b43f0e95e8ed3e386
Idelio-Mata <idelioteofilomata@gmail.com>
2022-12-13 17:46:29 +0200

    Stylishing Login finished

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,26 @@
+import './login.css'
+
+import { Logo} from '../../components/Logo'
+
 export default function Login(){
   return(
-    <div>
-      <h1>Login  Page</h1>
+    <div className='login-container'>
+      <Logo/>
+
+      <form className='form'>
+        
+        <input 
+          type='email' 
+          placeholder='Type your email...'
+        />
+        
+        <input
+          type='password'
+          placeholder='********' autocomplete='on'
+        />
+
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }

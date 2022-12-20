0fbc5ce6e89889248853522a0253e0d18758529b
Idelio-Mata <idelioteofilomata@gmail.com>
2022-12-20 15:22:06 +0200

    Log out implemented.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,34 @@
 import './header.css';
 
+import { BiLogOut } from 'react-icons/bi'
+import { Link } from 'react-router-dom';
+
+import { auth } from '../../services/firebaseConnection'
+import { signOut } from 'firebase/auth'
+
 export function Header(){
+
+  async function handleLogout(){
+    await signOut (auth)
+  }
+
   return(
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className='admin-header'>
+      <nav className='nav-header'>
+        
+        <button onClick={handleLogout}>
+          <BiLogOut size={28} color="#DB2629"/>
+        </button>
+
+        <Link to="/admin">
+          Links
+        </Link>
+        
+        <Link to="/admin/social">
+          Social medias
+        </Link>
+
+      </nav>
+    </header>
   )
-}
+} 
--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,7 +1,51 @@
+import './admin.css'
+import { Header } from '../../components/Header'
+import { Logo } from '../../components/Logo'
+import { Input } from '../../components/Input'
+import { MdAddLink }  from 'react-icons/md'
+
+
 export default function Admin(){
   return(
-    <div>
-      <h1>Admin Page</h1>
+    <div className='admin-container'>
+      <Header/>
+
+      <Logo/>
+
+      <form className='form'>
+
+        <label className='label'>Link name</label>
+        <Input 
+          placeholder="Type the link..."
+        />
+
+        <label className='label'>Link url</label>
+        <Input
+          type='url'
+          placeholder="Type the url..."
+        />
+
+        <section className='container-colors'>
+          
+          <div>
+            <label className='laber right'>Background link</label>
+            <input type="color"/>          
+          </div>
+
+          <div>
+            <label className='laber right'>Link color</label>
+            <input type="color"/>          
+          </div>
+
+        </section>
+
+        <button className='btn-register' type='submit'>
+          Register <MdAddLink size={24} color="#fff"/>
+        </button>
+
+
+      </form>
+
     </div>
   )
 }

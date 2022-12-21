754970e9f81530dcc3360bcc6b34beba4d2bd797
Idelio-Mata <idelioteofilomata@gmail.com>
2022-12-21 14:55:42 +0200

    Links personalizations created. Links previw rending onType.
    #Working in firestore to save data.

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,11 +1,20 @@
+import { useState } from 'react'
+
 import './admin.css'
 import { Header } from '../../components/Header'
 import { Logo } from '../../components/Logo'
 import { Input } from '../../components/Input'
 import { MdAddLink }  from 'react-icons/md'
+import { FiTrash2 } from 'react-icons/fi'
 
 
 export default function Admin(){
+  const [ nameInput, setNameInput] = useState("");
+  const [ urlInput, setUrlInput] = useState("");
+  const [ backgroundColorInput, setBackgroundColorInput] = useState("#f1f1f1");
+  const [ textColorInput, setTextColorInput] = useState("#121212");
+
+
   return(
     <div className='admin-container'>
       <Header/>
@@ -17,27 +26,49 @@ export default function Admin(){
         <label className='label'>Link name</label>
         <Input 
           placeholder="Type the link..."
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
         />
 
         <label className='label'>Link url</label>
         <Input
           type='url'
           placeholder="Type the url..."
+          value={urlInput}
+          onChange={(e) => setUrlInput(e.target.value)}
         />
 
         <section className='container-colors'>
           
           <div>
             <label className='laber right'>Background link</label>
-            <input type="color"/>          
+            <input 
+              type="color"
+              value={backgroundColorInput}
+              onChange={(e) => setBackgroundColorInput(e.target.value)}
+            />          
           </div>
 
           <div>
             <label className='laber right'>Link color</label>
-            <input type="color"/>          
+            <input 
+              type="color"
+              value={textColorInput}
+              onChange={(e) => setTextColorInput(e.target.value)}
+            />          
           </div>
-
+ 
         </section>
+
+        
+        {nameInput !== "" && (
+          <div className='preview'>
+            <label className='label'>See how its look 👇🏾</label>
+            <article className='list' style={{marginTop:8,marginBottom:8, backgroundColor:backgroundColorInput}}>
+              <p style={{color:textColorInput}}>{nameInput}</p>
+            </article>    
+          </div>
+        )}
 
         <button className='btn-register' type='submit'>
           Register <MdAddLink size={24} color="#fff"/>
@@ -49,8 +80,16 @@ export default function Admin(){
         My links
       </h2>
 
-      <article className='list'>
-        
+      <article 
+      className='list animate-pop'
+      style={{ backgroundColor: "#000", color: "#fff" }}
+      >
+        <p>Vip group in Telegram</p>
+        <div>
+          <button className='btn-delete'>
+            <FiTrash2 size={18} color="#fff"/>
+          </button>
+        </div>
       </article>
 
     </div>
